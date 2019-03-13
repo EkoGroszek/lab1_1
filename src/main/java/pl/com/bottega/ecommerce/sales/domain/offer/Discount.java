095aba5588efdf34f7cc2d9fe2c99119873af958
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-13 18:42:49 +0100

    Discount class content added

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,5 +1,27 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
 public class Discount {
+    private Money value;
+    private String cause;
 
+    public Discount(Money value, String cause) {
+        this.value = value;
+        this.cause = cause;
+    }
+
+    public Money getValue() {
+        return value;
+    }
+
+    public void setValue(Money value) {
+        this.value = value;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
 }
